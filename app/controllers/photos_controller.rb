b54023a57a8a47d67c35d26ab564c29552f88d3e
KatarinaLackner <Katarina.Lackner@gmail.com>
2016-11-08 14:30:23 -0600

    phase 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,37 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
 
+  def show
 
+    @photo = Photo.find_by({ :id => params[:id] })
+    @photo_source = @photo.source
+    @photo_caption = @photo.caption
+    @photo_id = @photo.id
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    #render("create_row.html.erb")
+    redirect_to("localhost:3000/photos")
+
+  end
+
+  def destroy
+
+    
+
+    end
 end
